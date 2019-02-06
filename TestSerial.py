2b090e38eafec9b50e5a53ea0d4550946fd15ebe
Pedro <pedrolfm@gmail.com>
2019-02-06 15:23:41 -0500

    Incomplete Version

--- TestSerial.py
+++ TestSerial.py
@@ -1,5 +1,113 @@
 import serial
 import time
+import rospy
+
+class Controller:
+
+    def __init__(self):
+        # Define the variables
+        self.CartesianPositionA = 0
+        self.CartesianPositionB = 0
+        self.OrientationA = 0
+        self.OrientationB = 0
+
+    def OpenConnection(self):
+        try:
+            self.ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)  # open serial port
+            self.ser.open()
+        except:
+            print("\n*** could not open the serial communication ***\n")
+            return 0
+        if self.ser.is_open():
+            return 1
+        else:
+            return 0
+
+    def SendMovementInCounts(self,X,Channel):
+        try:
+            self.ser.write(str("PR%s%d\r" % (Channel,X)))
+        except:
+            print("*** could not send command ***")
+            return 0
+
+        time.sleep(0.05)
+        self.ser.write(str("PR%s?\r" % Channel))
+        time.sleep(0.01)
+        bytesToRead = self.ser.inWaiting()
+        data_temp = self.ser.read(bytesToRead)
+        #print(data_temp)
+        if data_temp == X:
+            return 1
+        else:
+            print("*** could not properly send command ***")
+            return 0
+
+    def ExecMotion(self):
+        try:
+            self.ser.write(str("BG \r"))
+            time.sleep(0.01)
+            self.ser.write(str("PR 0,0,0,0 \r"))
+        except:
+            print("*** could not send command ***")
+            return 0
+
+    #TODO:
+    def CheckController(self):
+        try:
+            self.ser.write(str("MT ?,?,?,?\r"))
+            bytesToRead = self.ser.inWaiting()
+            data_temp = self.ser.read(bytesToRead)
+
+        except:
+            print("*** could not send command ***")
+            return 0
+
+    def SetAbsoluteMotion(self,Channel):
+        try:
+            self.ser.write(str("DP%s 0\r" % Channel))
+            self.ser.write(str("PT%s 1\r" % Channel))
+            self.AbsoluteMode = True
+            return 1
+        except:
+            print("*** could not set Absolute mode ***")
+            return 0
+
+    def SendAbsolutePosition(self,Channel,X):
+        try:
+            if self.AbsoluteMode == True:
+                self.ser.write(str("PA%s %d\r" % Channel,X))
+                return 1
+            else:
+                print("*** PA not available ***")
+                return 0
+
+        except:
+            print("*** could not send command ***")
+            return 0
+
+    def InitiUSmotors(self):
+        try:
+            self.ser.write(str("PR 1000,1000\r")) #Check the values depending on the hardware
+            self.ser.write(str("BG \r"))
+            time.sleep(1)
+            LRA = 0
+            LRB = 0
+            while LRA==0 and LRB==0:
+                self.ser.write(str("MG_LRA \r"))
+                bytesToRead = self.ser.inWaiting()
+                LRA = self.ser.read(bytesToRead)
+                self.ser.write(str("MG_LRB \r"))
+                bytesToRead = self.ser.inWaiting()
+                LRB = self.ser.read(bytesToRead)
+            self.ser.write(str("DPA 0\r"))
+            self.ser.write(str("DPB 0\r"))
+            return 1
+        except:
+            print("*** could not initialize motors ***")
+            return 0
+
+
+
 
 def main():
     try:
